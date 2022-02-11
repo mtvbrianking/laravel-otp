6187a379c2f6875d67118bcf754dcab37ae80e90
Brian Matovu <mtvbrianking@gmail.com>
2022-02-11 21:42:45 +0300

    Add google 2fa fields

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,6 +31,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'google2fa_secret',
     ];
 
     /**
@@ -40,5 +41,6 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'google2fa_enabled' => 'boolean',
     ];
 }
